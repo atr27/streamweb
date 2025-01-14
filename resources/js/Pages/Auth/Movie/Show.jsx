f915bb3097345810e2c78f54c9b84025ecefc1cb
atr27 <2701ator@gmail.com>
2025-01-15 01:46:51 +0700

    feat : edit slicing show movie

--- resources/js/Pages/Auth/Movie/Show.jsx
+++ resources/js/Pages/Auth/Movie/Show.jsx
@@ -5,32 +5,33 @@ export default function Show() {
     return (
         <>
             <section
-                class="watching-page relative mx-auto w-screen h-screen font-poppins bg-form-bg"
+                className="watching-page relative mx-auto w-screen h-screen font-poppins bg-form-bg overflow-hidden"
                 id="stream"
             >
-               <div className="pt-[100px]">
-               <ReactPlayer
-                    url={"https://www.youtube.com/watch?v=mqqft2x_Aa4"}
-                    controls
-                    width={"100%"}
-                    height={"835px"}
-               />
-               </div>
+                <div className="h-full pt-[90px] overflow-hidden">
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=mqqft2x_Aa4"
+                        controls
+                        width="100%"
+                        height="calc(100vh - 100px)"
+                        className="react-player"
+                    />
+                </div>
 
-                {/* <!-- Button back to dashboard --> */}
-                <div class="absolute left-5 top-5 z-20">
+                {/* Back to dashboard button */}
+                <div className="absolute left-5 top-5 z-20">
                     <Link href={route("auth.dashboard")}>
                         <img
                             src="/icons/ic_arrow-left.svg"
-                            class="btn-back w-[46px] transition-all"
-                            alt="stream"
+                            className="btn-back w-[46px] transition-all"
+                            alt="Back to dashboard"
                         />
                     </Link>
                 </div>
 
-                {/* <!-- Video Title --> */}
-                <div class="title-video absolute left-1/2 top-7 max-w-[310px] -translate-x-1/2 text-center md:max-w-[620px]">
-                    <span class="select-none text-2xl font-medium text-white drop-shadow-md transition-all">
+                {/* Video Title */}
+                <div className="title-video absolute left-1/2 top-7 max-w-[310px] -translate-x-1/2 text-center md:max-w-[620px]">
+                    <span className="select-none text-2xl font-medium text-white drop-shadow-md transition-all">
                         Details Screen Part Final
                     </span>
                 </div>
